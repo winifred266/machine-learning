11a71253f84cd37eb2fc5d0245cfb8ba47f545b5
winifred <winifredboahemaa99@gmail.com>
2024-07-25 14:19:42 -0700

    my profile

--- wini.py
+++ wini.py
@@ -1,5 +1,4 @@
-print("Welcome To DreamWorld")
-name = input("Enter your name: ")
-skill = input("Enter your skill: ")
-
-print(f'Hello {name} you are skilled in {skill}')
+print ("helo world")
+name = input("enter your place of birth\n ")
+year = input ("enter the year of birth\n ")
+print (f"your place of birth is {name} and the year of bith is {year}")
